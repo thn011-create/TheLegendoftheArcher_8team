576e8e43b76d81977b3b7fa92c9d6cd3c98f9f3a
thn011-create <thn011@naver.com>
2025-02-24 15:11:05 +0900

    [Feat : TH] GameManager 스크립트 추가

---
+++ Assets/01.Scripts/TH_Monster/GameManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public PlayerController player { get; private set; }
+    private ResourceController _playerResourceController;
+
+    [SerializeField] private int currentWaveIndex = 0;
+
+    private EnemyManager enemyManager;
+    private UIManager uiManager;
+    public static bool isFirstLoading = true;
+
+    private void Awake()
+    {
+        instance = this;
+        player = FindObjectOfType<PlayerController>();
+        player.Init(this);
+
+        uiManager = FindObjectOfType<UIManager>();
+
+        _playerResourceController = player.GetComponent<ResourceController>();
+        _playerResourceController.RemoveHealthChangeEvent(uiManager.ChangePlayerHP);
+        _playerResourceController.AddHealthChangeEvent(uiManager.ChangePlayerHP);
+
+        enemyManager = GetComponentInChildren<EnemyManager>();
+        enemyManager.Init(this);
+    }
+
+    private void Start()
+    {
+        if (!isFirstLoading)
+        {
+            StartGame();
+        }
+        else
+        {
+            isFirstLoading = false;
+        }
+    }
+
+    public void StartGame()
+    {
+        uiManager.SetPlayGame();
+        StartNextWave();
+    }
+
+    void StartNextWave()
+    {
+        currentWaveIndex += 1;
+        uiManager.ChangeWave(currentWaveIndex);
+        enemyManager.StartWave(1 + currentWaveIndex / 5);
+    }
+
+    public void EndOfWave()
+    {
+        StartNextWave();
+    }
+
+    public void GameOver()
+    {
+        enemyManager.StopWave();
+        uiManager.SetGameOver();
+    }
+}
+
