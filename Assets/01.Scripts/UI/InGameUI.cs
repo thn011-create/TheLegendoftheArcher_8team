3fb8f1bfea4c01f83fc300022c9e221fb777d4c4
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-24 16:09:11 +0900

    [Fix : SY] ArgumentException 오류 해결
    딕셔너리에 Add 메서드 대신 TryAdd로 변경

--- Assets/01.Scripts/UI/InGameUI.cs
+++ Assets/01.Scripts/UI/InGameUI.cs
@@ -7,7 +7,7 @@ public class InGameUI : BaseUI
     // Start is called before the first frame update
     void Start()
     {
-        uiState = UIState.SelectSkill;
+        uiState = UIState.InGame;
     }
 
     // Update is called once per frame
