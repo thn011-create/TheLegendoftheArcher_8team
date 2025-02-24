6b3091c590de4b16ebdccc92074e82565429bd31
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-25 00:21:39 +0900

    [Feat : SY] pasue UI 버튼연결
    이어서하기/홈으로가기 버튼 ui 연결
    (이어서하기- 보완필요/
    홈으로가기- 홈 미구현상태)

--- Assets/01.Scripts/SY_UI/PauseUI.cs
+++ Assets/01.Scripts/SY_UI/PauseUI.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseUI : BaseUI
 {
+    // 버튼
+    [SerializeField] Button continueBtn;
+    [SerializeField] Button exitBtn;
    
     void Start()
     {
         uiState = UIState.Pause;
+
+        continueBtn.onClick.AddListener(OnClickContinueButton);
+        continueBtn.onClick.AddListener(OnClickContinueButton);
     }
 
     
@@ -15,4 +23,20 @@ public class PauseUI : BaseUI
     {
         
     }
+
+    void OnClickContinueButton() 
+    {
+     
+        // 시간 멈춤?
+        // Time.timeScale = 0f;
+        // 팝업 끄고 게임 이어서 하기    
+        gameObject.SetActive(false); 
+    }
+
+    void OnClickExitButton() 
+    {
+        // 홈으로 나가기 (홈 미구현)
+        // UIState.HOME
+    }
+
 }
