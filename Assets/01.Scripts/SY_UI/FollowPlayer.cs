10e0d377e30109341dcc6f9c09422fd3f8e4f7d6
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-25 15:50:14 +0900

    [Fix: SY] HP바 null오류 해결
    스크립트 수정
    null 안뜨고 머리위에 잘뜸

--- Assets/01.Scripts/SY_UI/FollowPlayer.cs
+++ Assets/01.Scripts/SY_UI/FollowPlayer.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+    public Transform player;
+
+    GameManager gameManager;
     // 오브젝트 위치
     RectTransform rectTransform;
 
+    //public Vector3 offset = new Vector3(0, 1f, 0);
+
+    public float height = 0.1f;
+
     void Awake()
     {
+        
+
         rectTransform = GetComponent<RectTransform>();
+        if (rectTransform == null)
+            Debug.LogError("RectTransform NULL");
+        //DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         // 스크린 좌표로 플레이어의 위치를 변환해서 따라간다
-        rectTransform.position = Camera.main.WorldToScreenPoint(GameManager.instance.player.transform.position); 
-            
+
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(new Vector3(player.position.x, player.transform.position.y));
+        rectTransform.position = screenPoint;
+
+
     }
 }
