53b9c4f161f6dd61ee470b2dbeb04515362a7c74
Kim Seong Won <worni5015@gmail.com>
2025-02-26 12:03:29 +0900

    [Feat:KSW] Add MapPrefabs & Add MonsterWeapon

--- Assets/01.Scripts/KSW_Loaders/Map/MapDesign.cs
+++ Assets/01.Scripts/KSW_Loaders/Map/MapDesign.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class MapDesign : MonoBehaviour
 {
     [SerializeField] GameObject mapEdge;
+    [SerializeField] GameObject mapDoor;
     [SerializeField] List<GameObject> mapPrefabs;
+
+    [SerializeField] GameObject nextStageArea;
 
     public void SelectedMap(int minMapNum, int maxMapNum)
     {
@@ -14,9 +17,22 @@ public class MapDesign : MonoBehaviour
     public void DrawMap()
     {
         Instantiate(mapEdge);
+        Instantiate(mapDoor);
         for (int i = 1; i < 24; i+=3)
         {
             SelectedMap(i - 1, i + 2);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision == null)
+        {
+            return;
+        }
+        if (collision.CompareTag("Player"))
+        {
+
         }
     }
 }
