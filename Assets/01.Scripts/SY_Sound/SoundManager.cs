88af4108f6ac4d9e4da37fd0b5678154e69bba78
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-27 15:09:48 +0900

    [Feat : SY] SoundManager.cs 재생볼륨관련
    재생관리 및 playerPref으로 사용자 설정 저장

--- Assets/01.Scripts/SY_Sound/SoundManager.cs
+++ Assets/01.Scripts/SY_Sound/SoundManager.cs
@@ -7,17 +7,27 @@ public class SoundManager : MonoBehaviour
 {
     public static SoundManager instance;
 
+    // 볼륨,피치 조절
+    [SerializeField][Range(0f, 1f)] private float soundEffectVolume;
+    [SerializeField][Range(0f, 1f)] private float soundEffectPitchVariance;
+    [SerializeField][Range(0f, 1f)] private float musicVolume;
 
-    // 오디오믹스 - 사운드 조절 
-    [SerializeField] private AudioMixer audioMixer;
+    //// 오디오 믹서 - 사운드 조절 
+    //[SerializeField] private AudioMixer audioMixer;
     
-    // audio clip 담는 배열
-    [SerializeField] AudioClip[] bgms;
-    [SerializeField] AudioClip[] sfxs;
+    //// 오디오 클립 담는 배열
+    //[SerializeField] AudioClip[] bgms;
+    //[SerializeField] AudioClip[] sfxs;
 
-    // 플레이하는 audio source
-    [SerializeField] AudioSource playbgm;
-    [SerializeField] AudioSource playsfx;
+    //// 플레이 중인 오디오 소스
+    //[SerializeField] AudioSource playbgm;
+    //[SerializeField] AudioSource playsfx;
+
+    public  AudioClip musicCilp;
+    private AudioSource musicAudioSource;
+
+    // 사운드소스 프리팹
+    public SoundSource soundSourcePrefab;
 
     public enum SoundType
     {
@@ -25,6 +35,7 @@ public class SoundManager : MonoBehaviour
         SFX
     }
 
+    // 주의 : 정의한 내용과 할당한 클립의 순서가 같아야 함
     public enum Bgm
     {
         // BGM 총 3가지 예상
@@ -44,30 +55,51 @@ public class SoundManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
         else {  Destroy(gameObject); }
+
+        // bgm 볼륨,루프
+        musicAudioSource = GetComponent<AudioSource>();
+        musicAudioSource.volume = PlayerPrefs.GetFloat("MusicVolume", musicVolume);
+        musicAudioSource.loop = true;
+
     }
+
+    private void Start()
+    {
+        ChangeBackGroundMusic(musicCilp);
+    }
+
     
-    // bgm 재생 (enum Bgm 열거형)
-    public void PlayBGM(Bgm bgmIdx)
+    public void ChangeBackGroundMusic(AudioClip clip)
     {
+        if (musicAudioSource.clip == clip) return; // 중복재생 방지
 
+        musicAudioSource.Stop();
+        musicAudioSource.clip = clip;
+        musicAudioSource.Play();
     }
 
-    // sfx 재생 (enum Sfx 열거형)
-    public void PlaySFX(Sfx sfxIdx)
+    public static void PlayClip(AudioClip clip)
     {
-        // PLAY ONESHOT 이용
+        SoundSource obj = Instantiate(instance.soundSourcePrefab);
+        SoundSource soundSource = obj.GetComponent<SoundSource>();
+        soundSource.Play(clip, instance.soundEffectVolume, instance.soundEffectPitchVariance);
     }
 
-    // bgm 정지
-    public void StopBGM()
-    {
-        playbgm.Stop();
-    }
+    // 볼륨 //
 
-    // 소리 볼륨 조절 - 옵션
-    public void SetVolume(SoundType type, float value)
+    // bgm 
+    public void SetMusicVolume(float volume)
     {
-        audioMixer.SetFloat(type.ToString(), value);
+        musicVolume = volume;
+        musicAudioSource.volume = volume;
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+        PlayerPrefs.Save();
     }
-   
+    // sfx
+    public void SetSFXVolume(float volume)
+    {
+        soundEffectVolume = volume;
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+        PlayerPrefs.Save();
+    }
 }
