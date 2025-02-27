00f2586d1bf4801dbc7ab7187f469a79a0587586
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-28 03:46:29 +0900

    [Feat : SY] 아이템 획득시 SFX 출력
    획득 SFX 출력

--- Assets/01.Scripts/YJ_Player/ExperienceItem.cs
+++ Assets/01.Scripts/YJ_Player/ExperienceItem.cs
@@ -4,10 +4,13 @@ public class ExperienceItem : MonoBehaviour
 {
     public int experienceAmount = 10; // 경험치 양
 
+    public AudioClip ItemClip;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            SoundManager.PlayClip(ItemClip);
             collision.GetComponent<PlayerStats>().GainExperience(experienceAmount);
             Destroy(gameObject);
         }
