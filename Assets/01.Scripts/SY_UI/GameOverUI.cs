6898e7bba0d20b39c3d99a905f4fdb650d0bae8a
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-02-27 22:02:28 +0900

    ※[Merge : SY] UI합침

--- Assets/01.Scripts/SY_UI/GameOverUI.cs
+++ Assets/01.Scripts/SY_UI/GameOverUI.cs
@@ -2,27 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameOverUI : BaseUI
 {
     [SerializeField] Text bestStageText;
+    [SerializeField] Button closeBtn;
 
 
     private void Start()
     {
         uiState = UIState.GameOver;
+
+        closeBtn.onClick.AddListener(OnCloseButtonClick);
     }
     // 현재 스테이지 정보 가져오고, 저장
-    void OnEnable()
-    {
-        int currentStage = GameManager.instance.currentWaveIndex;
-        int bestStage = PlayerPrefs.GetInt("BestStage");
-        if (currentStage > bestStage)
-        {
-            PlayerPrefs.SetInt("BestStage", currentStage);
-            PlayerPrefs.Save();
-        }
+    //void OnEnable()
+    //{
+    //    //int currentStage = GameManager.instance.currentWaveIndex;
+    //    int bestStage = PlayerPrefs.GetInt("BestStage");
+    //    if (currentStage > bestStage)
+    //    {
+    //        PlayerPrefs.SetInt("BestStage", currentStage);
+    //        PlayerPrefs.Save();
+    //    }
 
-        bestStageText.text = "최고 기록: " + PlayerPrefs.GetInt("BestStage").ToString();
+    //    bestStageText.text = "최고 기록: " + PlayerPrefs.GetInt("BestStage").ToString();
+    //}
+
+    public void OnCloseButtonClick()
+    {
+        SceneManager.LoadScene("HomeScene");
     }
+
+
 }
